5474461b9ad6b78561e27ef678c0a2aff5d5fb6c
Edelyne Keisha <114095264+edeliciouso@users.noreply.github.com>
2022-12-21 01:26:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//Please put your answer here
+$(function() {
+    $(".signup-show").click(function(){
+      $("#signup-modal").fadeIn();
+    });
+});
+  
+$(function() {
+    $("#close-modal").click(function(){
+      $("#signup-modal").fadeOut();
+    });
+});
